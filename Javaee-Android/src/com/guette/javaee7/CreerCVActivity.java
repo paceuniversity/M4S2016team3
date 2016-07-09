955875d89482fc772aca4b7e73fc7ad2337ffa4a
Guette <murge1988@gmail.com>
2016-07-09 15:39:48 +0000

    syllabus

--- Javaee-Android/src/com/guette/javaee7/CreerCVActivity.java
+++
@@ -1,7 +0,0 @@
-package com.guette.javaee7;
-
-import android.app.Activity;
-
-public class CreerCVActivity extends Activity {
-
-}
--- Javaee-Android/src/com/guette/javaee7/ReponseOffreActivity.java
+++
@@ -1,7 +0,0 @@
-package com.guette.javaee7;
-
-import android.app.Activity;
-
-public class ReponseOffreActivity extends Activity {
-
-}
