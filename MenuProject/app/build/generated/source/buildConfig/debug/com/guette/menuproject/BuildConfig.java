bb239094a78cbeee18d943446b187603fe5951a6
Guette <murge1988@gmail.com>
2016-07-24 16:00:29 +0000

    a supp

---
+++ MenuProject/app/build/generated/source/buildConfig/debug/com/guette/menuproject/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.guette.menuproject;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.guette.menuproject";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
